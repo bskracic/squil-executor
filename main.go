a67a608d5361ce1f24849f1bbb4a7cd3d2a5a615
bskracic <borna.skracic7@gmail.com>
2023-06-07 18:34:27 +0200

    sync up to date

--- main.go
+++ main.go
@@ -8,8 +8,11 @@ import (
 
 	"github.com/bskracic/squil-executor/runner"
 	"github.com/bskracic/squil-executor/runtime"
-	"github.com/gorilla/mux"
-	"github.com/gorilla/sessions"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
 )
 
 type request struct {
@@ -33,67 +36,82 @@ type Table struct {
 
 const sessionName = "container-session"
 
-var store = sessions.NewCookieStore([]byte("secret-key-mega"))
-
 func main() {
 
 	runtime := runtime.NewDockerRuntime()
 	sqlRunner := runner.NewSqlRunner(runtime)
 
-	router := mux.NewRouter()
+	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowMethods = []string{"GET", "POST", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type"}
+	router.Use(cors.New(config))
 
-	router.HandleFunc("/exec", func(w http.ResponseWriter, r *http.Request) {
+	// Initialize cookie store
+	store := cookie.NewStore([]byte("dugacki-secret"))
 
+	// Set session options
+	store.Options(sessions.Options{
+		Path:     "/",
+		Domain:   "localhost",
+		MaxAge:   86400,
+		HttpOnly: true,
+		Secure:   false,
+		SameSite: http.SameSiteNoneMode,
+	})
+
+	// Use the cookie store for sessions
+	router.Use(sessions.Sessions(sessionName, store))
+
+	router.POST("/api/query", func(c *gin.Context) {
 		var req request
-		json.NewDecoder(r.Body).Decode(&req)
+		json.NewDecoder(c.Request.Body).Decode(&req)
 
-		id := GetContainerId(sqlRunner, w, r)
-
+		// id := GetContainerId(sqlRunner, c)
 		ctx := &runner.RunCtx{
-			ContId: id,
+			ContId: "ea16a132713364d8b0796541d07ab09e7374aed92f8e56273105096dcb912c4b",
 		}
 		res := sqlRunner.Run(ctx, req.Query, &runner.RunOptions{})
 
-		w.Header().Add("Content-Type", "application/json")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Set-Cookie")
 
-		json.NewEncoder(w).Encode(&response{
+		log.Println(res.Result)
+
+		c.JSON(200, &response{
 			Status:  string(res.Status),
 			Results: prepareResults(strings.Split(res.Result, "\n")),
 		})
-	}).Methods("POST")
+	})
 
-	log.Println("Listening on 1337")
-	log.Fatal(http.ListenAndServe(":1337", router))
+	log.Println("Server starting on port 1337...")
+	router.Run(":1337")
 }
 
-func GetContainerId(sr *runner.SqlRunner, w http.ResponseWriter, r *http.Request) string {
-	session, err := store.Get(r, sessionName) // Replace with your own session name
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return ""
-	}
+func GetContainerId(sr *runner.SqlRunner, c *gin.Context) string {
 
-	sessionId := session.Values["session_id"]
-
-	if val, ok := sessionId.(string); ok {
-		return val
-	} else {
+	session := sessions.Default(c)
+	id := session.Get("session_id")
+	if id == nil {
 		id := sr.CreateContainer()
-		session.Values["session_id"] = id
-		// Save session
-		err = session.Save(r, w)
-		if err != nil {
-			return " "
-		}
+		log.Printf("created container: %v\n", id)
+		session.Set("session_id", id)
+		session.Save()
 		return id
+	} else if value, ok := id.(string); ok {
+		return value
+	} else {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return ""
 	}
 }
 
 func prepareResults(lines []string) []QueryResult {
 	var results []QueryResult
+
 	for idx := 0; idx < len(lines); {
 		var result QueryResult
-		if strings.Contains(lines[idx], "-") { // we reached decorators, meaning that column names are before
+		if strings.HasPrefix(lines[idx], "-") { // we reached decorators, meaning that column names are before
 			// remove previous result info, because it was definition of columns
 			results = results[:len(results)-1]
 			result.Table = extractTable(lines, &idx)
